2bae0d525f36abfea51436cfbda4b8f790ff1d30
Flávio Dorta <dorta.dev@gmail.com>
2025-10-29 23:50:07 -0300

    Identifying Mutual Likes in a Dating App

---
+++ SQL/7 Kyu/IdentifyingMutualLikesInADatingApp.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT 
+    LEAST(ul1.liker_id, ul1.liked_id) AS user1_id,
+    GREATEST(ul1.liker_id, ul1.liked_id) AS user2_id
+FROM user_likes ul1
+JOIN user_likes ul2 
+    ON ul1.liker_id = ul2.liked_id 
+    AND ul1.liked_id = ul2.liker_id
+ORDER BY user1_id, user2_id;
