b987079fea3ec35b88d1626887bdac1a56ca0892
Flávio Dorta <dorta.dev@gmail.com>
2025-11-17 22:48:09 -0300

    bytes in string from ints

---
+++ SQL/7 Kyu/BytesInStringFromInts.sql
@@ -0,0 +1,7 @@
+SELECT
+  OCTET_LENGTH(number1::text) as octnum1,
+  OCTET_LENGTH(number2::text) as octnum2,
+  OCTET_LENGTH(number3::text) as octnum3,
+  OCTET_LENGTH(number4::text) as octnum4,
+  OCTET_LENGTH(number5::text) as octnum5
+FROM numbers
