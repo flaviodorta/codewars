8dd78bb73e9856f7b8993644d47aecf6f728c053
Flávio Dorta <dorta.dev@gmail.com>
2025-12-06 18:42:16 -0300

    js: rock paper scissors

---
+++ JavaScript/8 Kyu/RockPaperScissors.js
@@ -0,0 +1,15 @@
+const rps = (p1, p2) => {
+  if (p1 === p2) return 'Draw!';
+
+  const winsOver = {
+    rock: 'scissors',
+    scissors: 'paper',
+    paper: 'rock',
+  };
+
+  if (winsOver[p1] === p2) {
+    return 'Player 1 won!';
+  } else {
+    return 'Player 2 won!';
+  }
+};
